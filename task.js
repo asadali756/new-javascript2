9a237aed599de2e76a8118a37b3a65c8751b2f23
asadali <asadaliabcd67@gmail.com>
2024-01-24 16:13:49 +0500

    H.W

--- task.js
+++ task.js
@@ -1,43 +1,44 @@
+
+
+
+// //TURNARY OPERATION
+
 // let grade = parseInt(prompt("ENTER YOUR GRADE"))
-
-
-
-//TURNARY OPERATION
-
-let resullt = grade === "A" && per === 90 || grade === "B" && per === 80 || grade === "C" && per == 70 || grade === "D" && per === 60 ? "PASS" : "FAIL";
-console.log(resullt);
-function hello (){
-    console.log("abc");
-}
-hello()
-hello()
+// let per = parseFloat(prompt("ENTER YOUR PER"))
+// let resullt =  grade === "A" && per === 90 || grade === "B" && per === 80 || grade === "C" && per == 70 || grade === "D" && per === 60 ? "PASS" : "FAIL";
+// console.log(resullt);
+// function hello (){
+//     console.log("abc");
+// }
+// hello()
+// hello()
 
 
 
 
-function person (name , age ,grade){
-    return name + "" + age + "" +grade ;
+// function person (name , age ,grade){
+//     return name + "" + age + "" +grade ;
 
-}
-let myValues = person ("abc", "15", "b");
+// }
+// let myValues = person ("abc", "15", "b");
 
 
-function sum (a,b){
-    console.log(`THE SUM OF A + b ${a+b}`);
-}
-sum(50,25)
+// function sum (a,b){
+//     console.log(`THE SUM OF A + b ${a+b}`);
+// }
+// sum(50,25)
+
 
 
 let gameNum = 85;
-let userNum =prompt("ENTER YOUR NUMBER")
+let userNum = prompt("Enter Your Number")
 while(userNum!= gameNum){
-    userNum = prompt("YOUR GUESS THE WRONG NO: GUESS AGAIN!")
+    userNum = prompt("You Guess the wrong number : GUESS AGAIN PLZ")
 }
-
-Swal.fire({
-    title: "gAME aLERT!",
-    text: "you win the game",
-    timer: "5000"
+swal({
+    title: "CONGRATULATIONS!",
+    text: "WIN GAMES.",
+    imageUrl: "images/thumbs-up.jpg"
   });
 
 
